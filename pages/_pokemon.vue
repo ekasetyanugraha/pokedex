a89357763f41190c7c7a020dda8d32370433b23b
Eka Setya Nugraha <eka.setya@bukalapak.com>
2022-05-19 16:09:50 +0700

    Add Pokemon detail page

---
+++ pages/_pokemon.vue
@@ -0,0 +1,17 @@
+<template>
+  <b-container class="p-4">
+    <card-pokemon :pokemon="pokemon" class="mx-auto" style="width: 250px;" />
+  </b-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { URL_BASE_POKEAPI } from '~/config';
+
+export default Vue.extend({
+  async asyncData({ $axios, params }) {
+    const pokemon = await $axios.$get(`${URL_BASE_POKEAPI}/pokemon/${params.pokemon}`);
+    return { pokemon };
+  },
+})
+</script>
