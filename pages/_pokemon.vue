4ec161191c854cd806d30e31f265cfbb569156f4
Eka Setya Nugraha <eka.setya@bukalapak.com>
2022-05-19 21:21:17 +0700

    Show Pokemon's evolution chain

--- pages/_pokemon.vue
+++ pages/_pokemon.vue
@@ -1,10 +1,22 @@
 <template>
   <b-container class="p-4">
-    <card-pokemon
-      :pokemon="pokemon"
-      class="mx-auto"
-      style="width: 250px;"
-    />
+    <b-nav>
+      <b-nav-item>
+        <nuxt-link to="/">Back</nuxt-link>
+      </b-nav-item>
+    </b-nav>
+    <b-row>
+      <b-col v-for="pokemon in pokemonDetails" :key="`col-${pokemon.id}`">
+        <nuxt-link :to="pokemon.name" class="text-decoration-none text-body">
+          <card-pokemon
+            :pokemon="pokemon"
+            :is-active="pokemon.name === params.pokemon"
+            class="mx-auto"
+            style="width: 320px;"
+          />
+        </nuxt-link>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
@@ -12,10 +24,32 @@
 import Vue from 'vue'
 import { URL_BASE_POKEAPI } from '~/config';
 
+let temp: any[] = [];
+
+const getSpeciesChain = (chain: any) => {
+  temp.push(chain.species.name);
+
+  if (chain.evolves_to.length) {
+    getSpeciesChain(chain.evolves_to[0])
+  }
+
+  return temp;
+}
+
 export default Vue.extend({
   async asyncData({ $axios, params }) {
-    const pokemon = await $axios.$get(`${URL_BASE_POKEAPI}/pokemon/${params.pokemon}`);
-    return { pokemon };
+    temp = [];
+    const species = await $axios.$get(`${URL_BASE_POKEAPI}/pokemon-species/${params.pokemon}`);
+    const evolutionChain = await $axios.$get(species.evolution_chain.url);
+    const speciesChain = getSpeciesChain(evolutionChain.chain);
+    const pokemonDetails = await Promise.all(speciesChain.map(species => $axios.$get(`${URL_BASE_POKEAPI}/pokemon/${species}`)));
+
+    return {
+      params,
+      species,
+      speciesChain,
+      pokemonDetails,
+    };
   },
 })
 </script>
