efca541ded7f7ca9e18590a503ee308735412c0b
Eka Setya Nugraha <eka.setya@bukalapak.com>
2022-05-19 16:08:01 +0700

    Pokemon list

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,40 @@
 <template>
-  <Tutorial />
+  <b-container class="p-4">
+    <b-row cols="4">
+      <b-col
+        v-for="(pokemon, i) in pokemons"
+        :key="pokemon.url"
+        class="mt-4"
+      >
+        <nuxt-link :to="pokemon.name">
+          <b-card :title="`#${i}`" class="text-capitalize">{{ pokemon.name }}</b-card>
+        </nuxt-link>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { URL_BASE_POKEAPI } from '~/config';
 
 export default Vue.extend({
   name: 'IndexPage',
+  data: () => ({
+    pokemons: [],
+  }),
+  created() {
+    this.getPokemonList();
+  },
+  methods: {
+    async getPokemonList() {
+      try {
+        const { next, results } = await this.$axios.$get(`${URL_BASE_POKEAPI}/pokemon`);
+        this.pokemons = results;
+      } catch (error) {
+
+      }
+    },
+  },
 })
 </script>
