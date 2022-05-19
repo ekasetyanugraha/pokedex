5fe4332168caf615248ccc9d9a85557813c20572
Eka Setya Nugraha <eka.setya@bukalapak.com>
2022-05-19 21:20:24 +0700

    Change to official artwork on CardPokemon, add props isActive

--- components/CardPokemon.vue
+++ components/CardPokemon.vue
@@ -1,24 +1,28 @@
 <template>
   <b-card
     :title="`#${pokemon.id} - ${pokemon.name}`"
-    :img-src="pokemon.sprites.front_default"
+    :img-src="getOfficialArtwork(pokemon.id)"
+    :border-variant="isActive ? 'primary' : ''"
     class="text-capitalize"
   >
     <badges-pokemon-types :types="typesExtracted" />
 
-    <div
-      v-for="(stat, i) in pokemon.stats"
-      :key="`stat-${i}`"
-      class="mt-3"
-    >
-      <div>{{ stat.stat.name }}</div>
-      <b-progress :value="stat.base_stat" :max="255" show-value />
+    <div v-if="isActive">
+      <div
+        v-for="(stat, i) in pokemon.stats"
+        :key="`stat-${i}`"
+        class="mt-3"
+      >
+        <div>{{ stat.stat.name }}</div>
+        <b-progress :value="stat.base_stat" :max="255" show-value />
+      </div>
     </div>
   </b-card>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import { URL_BASE_OFFICIAL_ARTWORK } from '~/config';
 
 export default Vue.extend({
   props: {
@@ -26,10 +30,19 @@ export default Vue.extend({
       type: Object,
       required: true,
     },
+    isActive: {
+      type: Boolean,
+      default: false,
+    },
   },
   computed: {
     typesExtracted() {
       return this.pokemon.types.map((type: { type: any; }) => type.type);
+    },
+  },
+  methods: {
+    getOfficialArtwork(index: number) {
+      return `${URL_BASE_OFFICIAL_ARTWORK}/${index}.png`;
     },
   },
 })
