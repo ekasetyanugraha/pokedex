713fad2c9657507f80cc8e7fe5b30145f8a8c18e
Eka Setya Nugraha <eka.setya@bukalapak.com>
2022-05-19 17:22:27 +0700

    Implement infinite scroll

--- pages/index.vue
+++ pages/index.vue
@@ -7,7 +7,7 @@
         class="mt-4"
       >
         <nuxt-link :to="pokemon.name">
-          <b-card :title="`#${i}`" class="text-capitalize">{{ pokemon.name }}</b-card>
+          <b-card :title="`#${i+1}`" class="text-capitalize">{{ pokemon.name }}</b-card>
         </nuxt-link>
       </b-col>
     </b-row>
@@ -21,19 +21,72 @@ import { URL_BASE_POKEAPI } from '~/config';
 export default Vue.extend({
   name: 'IndexPage',
   data: () => ({
-    pokemons: [],
+    isLoading: false,
+    pokemons: [] as any,
+    next: '',
   }),
   created() {
     this.getPokemonList();
   },
+  mounted() {
+    this.attachScrollListener();
+  },
+  destroyed() {
+    this.detachScrollListener();
+  },
   methods: {
-    async getPokemonList() {
+    async getNextPokemonList() {
+      if (!this.next || this.isLoading) return;
+
+      this.isLoading = true;
       try {
-        const { next, results } = await this.$axios.$get(`${URL_BASE_POKEAPI}/pokemon`);
-        this.pokemons = results;
+        const { next, results } = await this.$axios.$get(this.next);
+
+        this.pokemons = [...this.pokemons, ...results];
+        this.next = next;
       } catch (error) {
 
+      } finally {
+        this.isLoading = false;
       }
+    },
+    async getPokemonList() {
+      if (this.isLoading) return;
+
+      this.isLoading = true;
+      try {
+        const { next, results } = await this.$axios.$get(`${URL_BASE_POKEAPI}/pokemon`, {
+          params: {
+            limit: 50,
+            offset: 0,
+          },
+        });
+
+        this.pokemons = results;
+        this.next = next;
+      } catch (error) {
+
+      } finally {
+        this.isLoading = false;
+      }
+    },
+    onPageScroll()  {
+      const { pageYOffset, innerHeight } = global;
+      const { offsetTop, offsetHeight } = document.documentElement;
+
+      const bottomWindowOffset = pageYOffset + innerHeight;
+      const bottomTabLimit = offsetTop + offsetHeight - 50;
+      const reachedBottom = bottomWindowOffset >= bottomTabLimit;
+
+      if (reachedBottom) {
+        this.getNextPokemonList();
+      }
+    },
+    attachScrollListener()  {
+      global.addEventListener('scroll', this.onPageScroll);
+    },
+    detachScrollListener()  {
+      global.removeEventListener('scroll', this.onPageScroll);
     },
   },
 })
